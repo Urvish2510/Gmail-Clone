fe9e6e586b2f54db6f1dc88bc5691be3f27ee98c
Urvish Patel <uvpatel2510@gmail.com>
2021-11-21 16:48:10 +0530

    Header file added

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import './Header.css';
 import MenuIcon from '@mui/icons-material/Menu';
-import { IconButton } from '@mui/material';
+import { Avatar, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Header() {
     return (
         <div className="header">
-            <div className="hader__left">
+            <div className="header__left">
                 <IconButton>
                     <MenuIcon />
                 </IconButton>
+                <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" alt="" />
             </div>
 
-            <div className="hader__middle">
-
+            <div className="header__middle">
+                <SearchIcon />
+                <input placeholder="Search mail" type="text" />
+                <ArrowDropDownIcon className="header__inputCaret"/>
             </div>
 
-            <div className="hader__right">
-
+            <div className="header__right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar />
             </div>
 
         </div>
