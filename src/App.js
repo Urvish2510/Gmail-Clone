9623924359927dbdb2016b214345ba310bbf5989
Urvish Patel <uvpatel2510@gmail.com>
2021-11-26 22:24:31 +0530

    Completed sidebar section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Mail from "./Mail.js";
+import EmailList from "./EmailList";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />  
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+
+        <div className="app__body">
+          <Sidebar />
+          <Routes>
+            <Route exact path="/mail" element={<Mail />} />
+            <Route exact path="/" element={<EmailList />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,26 +1,55 @@
-import { Button } from '@mui/material';
-import React from 'react';
-import './Sidebar.css';
-import AddIcon from '@mui/icons-material/Add';
-import InboxIcon from '@mui/icons-material/Inbox';
-import SidebarOption from './SidebarOption';
-import StarIcon from '@mui/icons-material/Star';
+import { Button, IconButton } from "@mui/material";
+import React from "react";
+import "./Sidebar.css";
+import AddIcon from "@mui/icons-material/Add";
+import InboxIcon from "@mui/icons-material/Inbox";
+import SidebarOption from "./SidebarOption";
+import StarIcon from "@mui/icons-material/Star";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import NearMeIcon from "@mui/icons-material/NearMe";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import PersonIcon from "@mui/icons-material/Person";
+import DuoIcon from "@mui/icons-material/Duo";
+import PhoneIcon from "@mui/icons-material/Phone";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <Button
-                className="sidebar__compose"
-                startIcon={<AddIcon fontSize="large" />}
-            >
-                Compose
-            </Button>
+  return (
+    <div className="sidebar">
+      <Button
+        className="sidebar__compose"
+        startIcon={<AddIcon fontSize="large" />}
+      >
+        Compose
+      </Button>
 
-            <SidebarOption Icon={InboxIcon} title="Inbox" number={11} 
-            selected={true}/>
-            <SidebarOption Icon={StarIcon} title="Starred" number={11} />
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={11}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={11} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={11} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={11} />
+      <SidebarOption Icon={NearMeIcon} title="Drafts" number={11} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={11} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
