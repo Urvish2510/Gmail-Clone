bb980ffa9de0fcb02903504471419ec073bafbb9
Urvish Patel <uvpatel2510@gmail.com>
2021-11-29 13:30:17 +0530

    Started SendMail component

--- src/SendMail.js
+++ src/SendMail.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import "./SendMail.css"
+import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import "./SendMail.css";
+import { Button } from "@mui/material";
 
 function SendMail() {
-    return (
-        <div>
-            
+  return (
+    <div className="sendMail">
+      <div className="sendMail__header">
+        <h3>New Message</h3>
+        <CloseIcon className="sendMail__close" />
+      </div>
+
+      <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+
+        <div className="sendMail__options">
+          <Button
+            className="sendMail__send"
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Send
+          </Button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
 
 export default SendMail;
