80a76c8527898ded9372131572704969f5d6fcf3
Urvish Patel <uvpatel2510@gmail.com>
2021-12-16 14:40:28 +0530

    Recover Completed

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/compat/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { App } from "./firebase.config";
+import { App } from "./firebase.config.js";
 
 const firebaseApp = firebase.initializeApp(App);
 
